31cb82b71471f4db214df67553c96a5b06759701
taha <taha.hachidi@gmail.com>
2026-04-16 02:06:04 +0100

    prepare front for azure static app

--- nexora-digital-frontend/src/lib/api.js
+++ nexora-digital-frontend/src/lib/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:8080/api',
+  baseURL: import.meta.env.VITE_API_BASE_URL || 'https://cpmrick-gubcd5fafwc5grcb.francecentral-01.azurewebsites.net/api',
   headers: {
     'Content-Type': 'application/json'
   }
